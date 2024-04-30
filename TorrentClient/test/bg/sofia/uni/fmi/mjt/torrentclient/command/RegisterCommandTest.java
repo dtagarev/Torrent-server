baf0eaea417c05ba94ce99b1bcb0d47e3f1f25e7
dtagarev <dimitar45@tagarev.simplelogin.com>
2024-04-30 17:49:19 +0300

    added tests For Register command

--- TorrentClient/test/bg/sofia/uni/fmi/mjt/torrentclient/command/RegisterCommandTest.java
+++ TorrentClient/test/bg/sofia/uni/fmi/mjt/torrentclient/command/RegisterCommandTest.java
@@ -1,31 +1,72 @@
 package bg.sofia.uni.fmi.mjt.torrentclient.command;
 
+import bg.sofia.uni.fmi.mjt.shared.exceptions.InvalidCommand;
 import bg.sofia.uni.fmi.mjt.torrentclient.directory.UserDirectory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class RegisterCommandTest {
-    @Mock
-    UserDirectory userDirectory;
 
-    @InjectMocks
     RegisterCommand registerCommand = new RegisterCommand();
 
     @Test
     void testRegisterCommand() {
-        when(UserDirectory.isAFile(any())).thenReturn(true);
+        try(MockedStatic<UserDirectory> userDirMock = Mockito.mockStatic(UserDirectory.class)) {
+            userDirMock.when(() -> UserDirectory.isAFile(any())).thenReturn(true);
+            List<String> args = List.of("user1", "file1,file2");
+            assertEquals("register user1 file1,file2",registerCommand.execute(args));
+        }
+    }
 
-        List<String> args = List.of("user1", "file1,file2");
-        assertEquals("register user1 file1,file2",registerCommand.execute(args));
+    @Test
+    void testRegisterCommandWithInvalidUsername() {
+        try(MockedStatic<UserDirectory> userDirMock = Mockito.mockStatic(UserDirectory.class)) {
+            userDirMock.when(() -> UserDirectory.isAFile(any())).thenReturn(true);
+            List<String> args = List.of("user1@", "file1,file2");
+            assertThrows(InvalidCommand.class, () -> registerCommand.execute(args));
+        }
+    }
+
+    @Test
+    void testRegisterCommandWithInvalidFile() {
+        try(MockedStatic<UserDirectory> userDirMock = Mockito.mockStatic(UserDirectory.class)) {
+            userDirMock.when(() -> UserDirectory.isAFile(any())).thenReturn(false);
+            List<String> args = List.of("user1", "file1,file2");
+            assertThrows(InvalidCommand.class, () -> registerCommand.execute(args));
+        }
+    }
+
+    @Test
+    void testRegisterCommandWithInvalidNumberOfArguments() {
+        try(MockedStatic<UserDirectory> userDirMock = Mockito.mockStatic(UserDirectory.class)) {
+            userDirMock.when(() -> UserDirectory.isAFile(any())).thenReturn(true);
+            List<String> args = List.of("user1");
+            assertThrows(InvalidCommand.class, () -> registerCommand.execute(args));
+
+            List<String> args2 = List.of("user1", "file1", "file2");
+            assertThrows(InvalidCommand.class, () -> registerCommand.execute(args2));
+        }
+    }
+
+    @Test
+    void testRegisterCommandWithInvalidCharacters() {
+        try(MockedStatic<UserDirectory> userDirMock = Mockito.mockStatic(UserDirectory.class)) {
+            userDirMock.when(() -> UserDirectory.isAFile(any())).thenReturn(true);
+            List<String> args = List.of("user1", "file1,file2$");
+            assertThrows(InvalidCommand.class, () -> registerCommand.execute(args));
+        }
     }
 }
